5e19ec5c52f99436ab7dc98106548ac9b11528d2
jagadishkatam <jagadish.katam@gmail.com>
2025-01-21 22:58:45 -0500

    initial commit

--- shiny.py
+++ shiny.py
@@ -315,7 +315,7 @@ with st.container():
             
         # upload1 = upload_files[0]
         # upload2 = upload_files[1]
-        processed_df, unique_trt = preprocess_data(upload1, upload2)
+        processed_df, unique_trt = preprocess_data(data1, data2)
         selected_trt = st.sidebar.multiselect("Select Treatment to Display", options=unique_trt, default=unique_trt)
         # Filter the data based on selected treatments
         df2 = processed_df[processed_df['TRT01P'].isin(selected_trt)]
